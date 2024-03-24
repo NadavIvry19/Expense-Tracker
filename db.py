302a5bfd1050ed0314563137cc855258d5a4d219
NadavIvry <nadavivry54@gmail.com>
2024-03-24 17:26:07 +0200

    please workkkk

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import os
 #Testcommitfromvscode
 # Connect to the MongoDB server running on localhost at default port 27017
-mongodb_uri = os.getenv('MONGODB_URI', 'mongodb://mongo:27017/')
+mongodb_uri = os.getenv('MONGODB_URI', 'mongodb://root:shalev@mongo-mongodb:27017/')
 client = MongoClient(mongodb_uri)
 
 # Access a specific database, create it if not exists
